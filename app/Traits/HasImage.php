fcffff11cdf9d0f812e97cb8c010bbb61e247779
Nikos <nksglns@gmail.com>
2021-11-02 00:57:14 +0200

    Added a trait to load image paths for teams & sports

---
+++ app/Traits/HasImage.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * A trait to add the image attribute on models on demand
+ */
+
+namespace App\Traits;
+
+trait HasImage
+{
+
+
+    /**
+     * This will add the extra "image" attribute
+     * to the appends array of the model
+     *
+     * @return void
+     */
+    public function initializeHasImage()
+    {
+        $this->append('image');
+    }
+
+    /**
+     * Sets the image attribute if an image exists for
+     * this model instance
+     *
+     * @return string
+     */
+    public function getImageAttribute()
+    {
+        $className = $this->getClassName();
+        $filename = storage_path('app/public/images/' . $className . '/' . $this->id . '.png');
+        if (file_exists($filename)) {
+            return asset('public/images/' . $className . '/' . $this->id . '.png');
+        } else {
+            return asset('assets/images/' . $className . '_default.png');
+        }
+    }
+}
