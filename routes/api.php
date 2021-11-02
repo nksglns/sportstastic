e002a1ded207e4f259d75d6f9de30135f55332f3
Nikos <nksglns@gmail.com>
2021-11-02 22:41:35 +0200

    Defined the routes for the API and frontend

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\SportController;
+use App\Http\Controllers\Api\TeamController;
+use App\Http\Controllers\Api\LeagueController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/sports/', [SportController::class, 'index']);
+Route::get('/sports/{sportSlug}', [SportController::class, 'leaguesBySport']);
+Route::get('/leagues/{leagueSlug}', [LeagueController::class, 'teamsByLeague']);
+Route::get('/teams/{teamSlug}', [TeamController::class, 'show']);
