f730eabb4500fce4bbdca7999042b98c339f20e1
Nikos <nksglns@gmail.com>
2021-11-02 22:43:24 +0200

    Added required frontend npm packages, prepared laravel mix config

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
 window._ = require('lodash');
+window.NProgress = require('nprogress');
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -8,21 +9,8 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
+window.axios.defaults.baseURL = window.baseURL;
+
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
 
-// import Echo from 'laravel-echo';
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
